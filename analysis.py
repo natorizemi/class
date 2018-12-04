764da235db59b6aa6302bede77fec4d8c389a953
mas9612 <mas9612@gmail.com>
2018-12-04 16:11:21 +0900

    migrate to analysis.py

--- analysis.py
+++ analysis.py
@@ -1,10 +1,40 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from numpy import *
-import os
 import copy
 
 
 class Analysis():
-    def __init__(self,data):
+    def __init__(self, data):
         pass
+
+    def analyze(self, docs):
+        # max_dfは0.5（半分以上の文書に出現する言葉はいらん）を設定
+        count_vectorizer = CountVectorizer(
+            input='content', max_df=0.5, min_df=1, max_features=3000,
+            encoding="shift_jis"
+        )
+        # 引数として、CountVectorizerとTfidfTransformerで使った双方の引数が指定できるようになっている
+        tfidf_vectorizer = TfidfVectorizer(
+            input='content', max_df=0.5, min_df=1, max_features=3000,
+            norm='l2', encoding="shift_jis"
+        )
+
+        # featue_name一覧を取得
+        features = count_vectorizer.get_feature_names()
+        # features[0:5]
+        # => ['0213', '10月25日', '13', '1980', '1989']
+
+        tfidf = tfidf_vectorizer.fit_transform(docs)
+        print(tfidf)
+
+        print("--")
+        for j in ['老人', '政治']:
+            word_result = tfidf.toarray()[:, features.index(j)]
+            word2_result = copy.deepcopy(word_result)
+            word_list = word_result.tolist()
+            test = sorted(word2_result, reverse=True)
+            positions = []
+            for j in test[:10]:
+                positions.append(word_list.index(j))
+
+            print(positions)
